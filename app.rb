ef15a2d48329e69da556875f83882eb40accc1ad
Lance Pollard <lancejpollard@gmail.com>
2010-04-19 02:38:25 -0700

    working decently

--- app.rb
+++ app.rb
@@ -1,9 +1,8 @@
-# http://neothemes.com/rofolio-demo/?cat=7
-# http://blacksteel.x10hosting.com/contact.html
 require 'rubygems'
 require 'haml'
 require 'sinatra'
 require 'broadway'
+require 'rest_client'
 
 set :public, "public"
 set :views, "views"
@@ -14,4 +13,22 @@ get "/" do
   options = {}
   locals = {:cache_root => CACHED_COMMONS}
   haml :index, options, locals
+end
+
+post "/minify" do
+  options = {
+    :compresstext => params["input_text"],
+    :type => params["compressor"] == "css" ? "CSS" : "JS"
+  }
+  response = RestClient.post "http://refresh-sf.com/yui/", options
+  html = Nokogiri::HTML(response.to_s)
+  minified = html.xpath("//textarea[@class='output']").first.text
+  stats = html.xpath("//dl[@class='stats']").first
+  stats_hash = {}
+  keys = stats.xpath("dt")
+  values = stats.xpath("dd")
+  keys.each_with_index do |child, index|
+    stats_hash[child.text] = values[index].text
+  end
+  minified
 end
--- public/application.js
+++ public/application.js
@@ -1,13 +1,35 @@
-$(document).ready(function() {
-	
+$(document).ready(function() {	
 	// variables
-	$input = $("#input");
-	$output = $("#output");
+	$input 				= $("#input");
+	$output 			= $("#output");
+	$minify_form 	= $("form");
+	$output_text 	= $("#output_text");
+	$loader 			= $("#loader");
+	$content			= $("#content");
 	
-	$("*").toggle(function() {
-		$input.animate({top:"5%"});
-	}, function() {
-		$input.animate({top:"40%"});
-		$output.slideDown();
-	})
-})
+//	$('input[type=checkbox], input[type=radio]').prettyCheckboxes();
+	
+	$('html, body').animate({opacity:1});
+	$("#main").delay(500).animate({opacity:1});
+	
+	$minify_form.ajaxForm({
+		beforeSubmit: function(arr, $form, options) {
+			$loader.slideDown();
+			$output.animate({opacity:0});
+		},
+		success: function(responseText, statusText, xhr, $form) {
+			updateForm(responseText.toString());
+		},
+		error: function(responseText, statusText, xhr, $form) {
+			updateForm(responseText.toString());
+		}
+	});
+});
+
+function updateForm(data) {
+	$loader.slideUp();
+	$input.animate({top:"4%"});
+	$output.delay(300).animate({opacity:1}, function() {
+		$output_text.val(data);
+	});
+}
