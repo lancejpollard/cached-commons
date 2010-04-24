44d518b50a85884d2d02d06e45bd1e66e46f0590
Lance Pollard <lancejpollard@gmail.com>
2010-04-24 16:54:33 -0700

    added many more libraries

--- _source/app.rb
+++ _source/app.rb
@@ -7,7 +7,15 @@ require 'rest_client'
 set :public, "public"
 set :views, "views"
 
-CACHE_ROOT = "http://viatropos.github.com/cached-commons"
+CACHE_ROOT = "http://cachedcommons.org"
+
+=begin
+before do
+  if request.env['HTTP_HOST'] != CACHE_ROOT
+    redirect_to CACHE_ROOT
+  end
+end
+=end
 
 get "/" do
   locals = default_locals(:page => site.pages.first, :cache_root => CACHE_ROOT)
